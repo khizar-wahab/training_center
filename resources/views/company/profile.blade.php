85c847e57acb53d3a72831f0a03dbfdfee944816
wadood <wanikhan919@gmail.com>
2022-11-03 18:07:13 +0500

    Wip Company

--- resources/views/company/profile.blade.php
+++ resources/views/company/profile.blade.php
@@ -2,17 +2,6 @@
 
 @section('content')
 <div class="container">
-    <div class="card">
-      <div class="row">
-        <div class="col-lg-4">
-          <img src="https://ld-wt73.template-help.com/intense/intense2/images/persons/person-04-510x510.jpg" alt="">
-          <a href="#" class="btn btn-primary">Company Profile</a>
-        </div>
-        <div class="col-lg-8">
-          <h1 dir="ltr">Name</h1>
-          <p dir="ltr">I’m a web designer/developer focused on crafting great web experiences. Designing has been my passion since the days I started working with computers but I found myself into web design since 2007. I enjoy creating beautifully designed, intuitive and functional websites.</p>
-        </div>
-      </div>
-    </div>
+
 </div>
 @endsection
