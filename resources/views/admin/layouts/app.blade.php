ff47c552e0b01658a0f746964266a552f3933855
ali9585 <114659707+ali9585@users.noreply.github.com>
2022-11-02 22:26:54 -0700

    admin update2

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -12,9 +12,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     {{-- form template --}}
-    <link rel="stylesheet" href="/assets/css/loginform/style.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/loginform/style.css') }}">
     {{-- custom --}}
-    <link rel="stylesheet" href="/assets/css/admin/style.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/admin/style.css') }}">
     @stack('css')
 
   </head>
@@ -29,7 +29,7 @@
       {{-- jquery --}}
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
       {{-- custom scripts --}}
-      <script src="/assets/js/admin/main.js"></script>
+      <script src="{{ asset('assets/js/admin/main.js') }}"></script>
 
     @endpush
     
